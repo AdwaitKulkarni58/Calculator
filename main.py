018adfbf18ca29073f23bb46d94fd7c628bc0ccc
Adwait <adwait.kul.2018@gmail.com>
2022-12-15 13:58:51 -0800

    Final touches

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 def add(a, b):
     return a + b
 
+
 def subtract(a, b):
     return a - b
+
 
 def multiply(a, b):
     return a * b
 
+
 def divide(a, b):
     return a / b
+
 
 operations = {
     "+": add,
@@ -17,25 +21,26 @@ operations = {
     "/": divide,
 }
 
-num1 = int(input("Enter the first number: "))
-    
-want_to_continue = True 
+def calculate():
+    num1 = int(input("Enter the first number: "))
+    want_to_continue = True
 
-while want_to_continue:
-    for operation in operations:
-        print(operation)
-        
-    user_choice = input("Choose an operation from these options: ")
-    
-    num2 = int(input("Enter the second number: "))
+    while want_to_continue:
+        for operation in operations:
+            print(operation)
 
-    function_chosen = operations[user_choice]
-    result = function_chosen(num1, num2)
-    
-    print(f"{num1} {user_choice} {num2} = {result}")
-    
-    calculate_again = input(f"Enter y to continue with {result}, or 'n' to stop calculation: ")
-    if (calculate_again == "y"):
-        num1 = result  
-    else:
-        want_to_continue = False
+        user_choice = input("Choose an operation from these options: ")
+
+        num2 = int(input("Enter the second number: "))
+
+        function_chosen = operations[user_choice]
+        result = function_chosen(num1, num2)
+
+        print(f"{num1} {user_choice} {num2} = {result}")
+
+        calculate_again = input(f"Enter y to continue with {result}, or 'n' to stop calculation: ")
+        if (calculate_again == "y"):
+            num1 = result
+        else:
+            want_to_continue = False
+calculate()
