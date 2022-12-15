8926a78ffa74ab563ebeaa5e9c3e932078dcbc66
Adwait <adwait.kul.2018@gmail.com>
2022-12-15 13:48:02 -0800

    Got functionality right finally

--- main.py
+++ main.py
@@ -18,29 +18,24 @@ operations = {
 }
 
 num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
+    
+want_to_continue = True 
 
-for operation in operations:
-    print(operation)
-
-while True:
-    user_choice = input("Choose an operation from the above options: ")
+while want_to_continue:
+    for operation in operations:
+        print(operation)
+        
+    user_choice = input("Choose an operation from these options: ")
+    
+    num2 = int(input("Enter the second number: "))
 
     function_chosen = operations[user_choice]
     result = function_chosen(num1, num2)
     
     print(f"{num1} {user_choice} {num2} = {result}")
-
-    new_operation = input("Choose another operation: ")
-
-    num3 = int(input("Enter another number: "))
-
-    new_operation_chosen = operations[new_operation]
-    new_result = new_operation_chosen(result, num3)
-
-    print(f"{result} {new_operation} {num3} = {new_result}")
-    calculate_again = input(f"Enter y to continue with {new_result}, or 'n' to stop calculation: ")
-    if (calculate_again == "n"):
-        break
+    
+    calculate_again = input(f"Enter y to continue with {result}, or 'n' to stop calculation: ")
+    if (calculate_again == "y"):
+        num1 = result  
     else:
-        continue
+        want_to_continue = False
